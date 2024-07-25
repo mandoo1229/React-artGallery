9c14f258ae1aa83cc507346df4788ecede263e5c
mandoo1229 <skaalsdn35@gmail.com>
2024-07-25 18:15:56 +0900

    Feat : 테이블 디자인 추가 및 이미지 테이블에 보여지도록함.

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import { API_URL } from "../config";
+import "../style/table.css";
+import axios from "axios";
 
 const Main = () => {
   const [imageList, setImageList] = useState([]);
@@ -23,7 +24,35 @@ const Main = () => {
 
   return (
     <div>
-      <div>메인페이지</div>
+      <table>
+        <caption>미술관 전시 목록</caption>
+        <thead>
+          <tr>
+            <th>NO</th>
+            <th>작품 이미지</th>
+            <th>작품명</th>
+          </tr>
+        </thead>
+        <tbody>
+          {imageList.map((imageList) => (
+            <tr style={{ textAlign: "center" }} key={imageList.id}>
+              <td>{imageList.id}</td>
+              <td>
+                <img
+                  style={{
+                    width: "200px",
+                    height: "200px",
+                    margin: "0 auto",
+                  }}
+                  src={`http://localhost:8080/upload/${imageList.saveNm}`}
+                />
+              </td>
+              <td>{imageList.orgNm}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <Stack direction="row" spacing={2}>
         <Button
           variant="contained"
