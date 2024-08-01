83ff9727e41a02e226bb97f01e9c2ac35828c9ea
mandoo1229 <skaalsdn35@gmail.com>
2024-08-01 16:59:23 +0900

    Feat : 회원가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Main from "./main/Main";
 import ImageView from "./main/image/ImageView";
 import ImageInsert from "./main/image/ImageInsert";
 import ImageMain from "./main/image/ImageMain";
+import SignUp from "./main/login/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/imageMain" element={<ImageMain />} />
         <Route path="/view/:id" element={<ImageView />} />
         <Route path="/upload" element={<ImageInsert />} />
