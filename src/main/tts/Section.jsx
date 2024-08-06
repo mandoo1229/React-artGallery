b69860fb018ede97219f3ae4a953b563bdd349f2
mandoo1229 <skaalsdn35@gmail.com>
2024-08-06 11:58:48 +0900

    Feat : 웹 화면 뜨면 자동으로 버튼 클릭하는 기능 추가

--- src/main/tts/Section.jsx
+++ src/main/tts/Section.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../style/main.css";
 
 const Section = ({ text, setText, convertTextToSpeech }) => {
+  useEffect(() => {
+    (async () => {
+      try {
+        window.addEventListener("click", function () {});
+        const btnEl = document.querySelector(".btn-convert");
+        setTimeout(() => {
+          btnEl.click();
+        }, 1000);
+      } catch (error) {
+        console.log("에러" + error);
+      }
+      return () => {
+        window.removeEventListener("click", function () {});
+      };
+    })();
+  }, []);
+
   return (
     <div className="section-container">
       <textarea value={text} onChange={(e) => setText(e.target.value)} />
